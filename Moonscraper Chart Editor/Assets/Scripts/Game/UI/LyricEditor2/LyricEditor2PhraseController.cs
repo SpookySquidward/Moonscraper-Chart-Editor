9da29a2841bf0d5638b6c2ed5a38e823c36e9d51
SpookySquidward <spookysquidward47@gmail.com>
2021-02-12 16:11:57 -0600

    Add LyricItem Class
    LyricItem will be used to represent all events handled by the lyric editor, including lyrics and phrase start/end events.

--- Moonscraper Chart Editor/Assets/Scripts/Game/UI/LyricEditor2/LyricEditor2PhraseController.cs
+++ Moonscraper Chart Editor/Assets/Scripts/Game/UI/LyricEditor2/LyricEditor2PhraseController.cs
@@ -1,9 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine.UI;
+using MoonscraperChartEditor.Song;
 
-public class LyricEditor2PhraseController : MonoBehaviour
+public class LyricEditor2PhraseController : UnityEngine.MonoBehaviour
 {
+    class LyricItem
+    {
+        public LyricEditorItemInterface lyricItemInterface;
+        public Event lyricItemEvent;
+        public string text = "";
+
+        public LyricItem(LyricEditorItemInterface lyricItemInterface, Event lyricEvent)
+        {
+            this.lyricItemInterface = lyricItemInterface;
+            this.lyricItemEvent = lyricEvent;
+        }
+
+        public void SetTime(uint tick)
+        {
+            List<SongEditCommand> commands = new List<SongEditCommand>();
+
+            if (lyricItemEvent != null)
+            {
+                commands.Add(new SongEditDelete(lyricItemEvent));
+            }
+
+            Event newLyric = new Event(this.text, tick);
+            commands.Add(new SongEditAdd(newLyric));
+
+            BatchedSongEditCommand batchedCommands = new BatchedSongEditCommand(commands);
+            ChartEditor.Instance.commandStack.Push(batchedCommands);
+
+            this.lyricItemEvent = newLyric;
+        }
+    }
+
+    List<LyricItem> syllables = new List<LyricItem>();
+
     // Start is called before the first frame update
     void Start()
     {
