e567e8d2e678c8a300048dea0210b8d4533f355c
SpookySquidward <spookysquidward47@gmail.com>
2021-02-12 20:53:40 -0600

    Add LyricEditor2Interface class
    The main front-end for the lyric editor system.  Added as a component to the New Lyric Editor Menu

---
+++ Moonscraper Chart Editor/Assets/Scripts/Game/UI/LyricEditor2/LyricEditor2Interface.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine.UI;
+using MoonscraperChartEditor.Song;
+
+public class LyricEditor2Interface : UnityEngine.MonoBehaviour
+{
+    [UnityEngine.SerializeField]
+    LyricEditor2PhraseController phraseTemplate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
