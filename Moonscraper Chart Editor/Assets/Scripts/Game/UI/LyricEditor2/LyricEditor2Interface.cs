0649c8809f8ebf0d2bcc3cd410ab37e2b3ee4b75
SpookySquidward <spookysquidward47@gmail.com>
2021-02-13 12:16:27 -0600

    Add ParseLyrics() Method
    This method parses raw user input into usable syllable information using regular expressions. User will be able to input lyrics in standard notation (syllables separated by dashes, phrases separated by newlines)

--- Moonscraper Chart Editor/Assets/Scripts/Game/UI/LyricEditor2/LyricEditor2Interface.cs
+++ Moonscraper Chart Editor/Assets/Scripts/Game/UI/LyricEditor2/LyricEditor2Interface.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using MoonscraperChartEditor.Song;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 public class LyricEditor2Interface : UnityEngine.MonoBehaviour
 {
@@ -16,5 +18,39 @@ public class LyricEditor2Interface : UnityEngine.MonoBehaviour
     void Update()
     {
 
+    }
+
+    // Parse a string into a double string array (phrases of syllables) to be
+    // given as Phrase Controller input. Does not have an implemented time-out
+    // period in case of excessively long strings to be parsed.
+    List<List<string>> ParseLyrics(string inputString)
+    {
+        // Start by splitting the string into phrases
+        char[] newlineCharacters = {'\n', '\r'};
+        string[] tempPhrases = inputString.Split(newlineCharacters, System.StringSplitOptions.RemoveEmptyEntries);
+
+        // Prepare the regex engine to parse each phrase
+        List<List<string>> parsedLyrics = new List<List<string>>();
+        // [^-\s]+      matches one or more characters in a syllable, excluding
+        //                  spaces and dashes
+        // (-\s?|\s?)   matches a dash, or whitespace if no dash is found
+        string regexPattern = @"[^-\s]+(-|\s?)";
+        Regex rx = new Regex(regexPattern);
+
+        foreach (string basePhrase in tempPhrases)
+        {
+            // Match each phrase
+            MatchCollection matches = rx.Matches(basePhrase);
+            // Convert the MatchCollection into a List and append that to
+            // parsedLyrics
+            List<string> matchesList = new List<string>();
+            for (int i = 0; i < matches.Count; i++)
+            {
+                matchesList.Add(matches[i].ToString());
+            }
+            parsedLyrics.Add(matchesList);
+        }
+
+        return parsedLyrics;
     }
 }
